ea32c0ee74eb5d317cf43ef01fa9e63d44087e8c
Skady17 <tarantinolashxi@gmail.com>
2021-05-27 00:57:07 +0400

    catalog

--- src/app/guest/catalog/catalog.component.ts
+++ src/app/guest/catalog/catalog.component.ts
@@ -6,7 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalog.component.scss']
 })
 export class CatalogComponent implements OnInit {
-
+    formebi = [
+      {
+        text: "NEWS",
+      },
+      {
+        text: "NEWS1",
+      },{
+        text: "NEWS2",
+      },{
+        text: "NEWS3",
+      },{
+        text: "NEWS4",
+      },{
+        text: "NEWS5",
+      },{
+        text: "NEWS6",
+      },{
+        text: "NEWS7",
+      },{
+        text: "NEWS8",
+      },{
+        text: "NEWS9",
+      },{
+        text: "NEWS10",
+      },{
+        text: "NEWS11",
+      },{
+        text: "NEWS12",
+      },{
+        text: "NEWS13",
+      },{
+        text: "NEWS14",
+      },{
+        text: "NEWS15",
+      },{
+        text: "NEWS16",
+      },{
+        text: "NEWS17",
+      },{
+        text: "NEWS18",
+      },{
+        text: "NEWS19",
+      },{
+        text: "NEWS20",
+      },
+    ]
+    
   constructor() { }
 
   ngOnInit(): void {
