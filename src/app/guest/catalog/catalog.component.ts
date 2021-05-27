cbda5236c8736a291fc954083aeda89b0313434e
Skady17 <tarantinolashxi@gmail.com>
2021-05-27 23:48:43 +0400

    catalog

--- src/app/guest/catalog/catalog.component.ts
+++ src/app/guest/catalog/catalog.component.ts
@@ -11,7 +11,7 @@ export class CatalogComponent implements OnInit {
         text: "NEWS",
       },
       {
-        text: "HelloWorld",
+        text: "HelloWorld!",
       },{
         text: "NEWS2",
       },{
