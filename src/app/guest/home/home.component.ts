819ce8bb43e32f892039ae50028d9efe1b0b1461
UchaBokeria <77641706+UchaBokeria@users.noreply.github.com>
2021-05-27 22:59:55 +0400

    x

--- src/app/guest/home/home.component.ts
+++ src/app/guest/home/home.component.ts
@@ -6,7 +6,141 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  post = [
+    {
+      id: 1,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post full",
+    },
+    {
+      id: 2,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post half",
+    },
+    {
+      id: 2,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post half",
+    },
+    {
+      id: 2,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post half",
+    },
+    {
+      id: 3,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post quarter",
+    },
+    {
+      id: 3,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post quarter",
+    },
+    {
+      id: 3,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post quarter",
+    },
+    {
+      id: 3,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post quarter test",
+    },
+    {
+      id: 2,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post half",
+    },
+    {
+      id: 4,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post half-height",
+    },
+    {
+      id: 4,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post half-height",
+    },
+    {   
+      id: 5,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post thierd",
+    },
+    {   
+      id: 5,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post thierd",
+    },
+    {   
+      id: 5,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post thierd",
+    },
+    {   
+      id: 6,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post forty",
+    },
+    {   
+      id: 5,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post thierd",
+    },
+    {   
+      id: 5,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post thierd",
+    },
+    {
+      id: 6,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post forty",
+    },
+    {
+      id: 7,
+      category:"Temporery",
+      description: "asdasdasdasdasdasdasd",
+      img: "asdasdasdasdasdasd",
+      deminsion: "home-post sixty",
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
